d90821367f8392f87f1a6cd0f2b9b2d7a42c8dfd
BM835 <devdev@bm835.space>
2019-07-26 23:27:37 +1000

    retry added

--- main.cs
+++ main.cs
@@ -15,44 +15,49 @@ namespace CSharp_Shell
         	string op;
         	int n2;
         	int ans = 0;
+        	string retry = "";
         	
-        	Console.Write("Input first number: ");
-        	input = Console.ReadLine();
-        	Int32.TryParse(input, out n1);
-        	
-        	Console.Write("Input operator: ");
-        	op = Console.ReadLine();
-        	
-        	Console.Write("Input second number: ");
-        	input = Console.ReadLine();
-        	Int32.TryParse(input, out n2);
-        	bool printAns = true;
-        	
-        	if(op == "+")
-        	{
-        		ans = n1 + n2;
-        	}
-        	if(op == "-")
-        	{
-        		ans = n1 - n2;
-        	}
-        	if(op == "*")
-        	{
-        		ans = n1 * n2;
-        	}
-        	if(op == "/")
-        	{
-        		if(n2 == 0){
-        			Console.WriteLine("Can't divide by 0");
-        			printAns = false;
-        		}else{
-        			ans = n1 / n2;
-        		}
-        	}
-        	
-        	if(printAns){
-        		Console.WriteLine(n1.ToString() + op + n2.ToString() + "=" + ans.ToString());
-        	}
+        	do{
+	        	Console.Write("Input first number: ");
+	        	input = Console.ReadLine();
+	        	Int32.TryParse(input, out n1);
+	        	
+	        	Console.Write("Input operator: ");
+	        	op = Console.ReadLine();
+	        	
+	        	Console.Write("Input second number: ");
+	        	input = Console.ReadLine();
+	        	Int32.TryParse(input, out n2);
+	        	bool printAns = true;
+	        	
+	        	if(op == "+")
+	        	{
+	        		ans = n1 + n2;
+	        	}
+	        	if(op == "-")
+	        	{
+	        		ans = n1 - n2;
+	        	}
+	        	if(op == "*")
+	        	{
+	        		ans = n1 * n2;
+	        	}
+	        	if(op == "/")
+	        	{
+	        		if(n2 == 0){
+	        			Console.WriteLine("Can't divide by 0");
+	        			printAns = false;
+	        		}else{
+	        			ans = n1 / n2;
+	        		}
+	        	}
+	        	
+	        	if(printAns){
+	        		Console.WriteLine(n1.ToString() + op + n2.ToString() + "=" + ans.ToString());
+	        	}
+	        	Console.Write("Retry? (y/n) ");
+	        	retry = Console.ReadLine();
+        	}while (retry == "y");
         }
     }
 }
